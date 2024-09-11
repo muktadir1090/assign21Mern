9fc4880acef814f64113bcf985f2bc46fbc672db
Tuba Muktadir <muktadir1090@gmail.com>
2024-09-11 17:03:48 -0400

    Added new files

---
+++ Develop/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+
+const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+
+const app = express();
+
+const startApolloServer = async () => {
+  await server.start();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+};
+startApolloServer();
