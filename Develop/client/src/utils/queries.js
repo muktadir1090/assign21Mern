bb79c823998da89993598c4aaf169d194955273a
Tuba Muktadir <muktadir1090@gmail.com>
2024-09-18 14:59:49 -0400

    update mutation and resolver

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,22 +1,13 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
-      _id
-      name
+export const QUERY_me = gql`
+  query me {
+    me {
+      _id 
+      username
+      email
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
+
