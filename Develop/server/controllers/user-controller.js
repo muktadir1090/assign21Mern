3ea666d4878d6d36c32c80d9813d46fd25152352
Tuba Muktadir <muktadir1090@gmail.com>
2024-09-18 18:04:32 -0400

    bug fix

--- Develop/server/controllers/user-controller.js
+++ Develop/server/controllers/user-controller.js
@@ -1,10 +1,10 @@
-// import user model
+// Import user model
 const { User } = require('../models');
-// import sign token function from auth
+// Import sign token function from auth
 const { signToken } = require('../utils/auth');
 
 module.exports = {
-  // get a single user by either their id or their username
+  // Get a single user by either their id or their username
   async getSingleUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
       $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
@@ -16,20 +16,25 @@ module.exports = {
 
     res.json(foundUser);
   },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+
+  // Create a user, sign a token, and send it back
   async createUser({ body }, res) {
     const user = await User.create(body);
 
     if (!user) {
       return res.status(400).json({ message: 'Something is wrong!' });
     }
+
     const token = signToken(user);
     res.json({ token, user });
   },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
+
+  // Login a user, sign a token, and send it back
   async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    const user = await User.findOne({ 
+      $or: [{ username: body.username }, { email: body.email }] 
+    });
+
     if (!user) {
       return res.status(400).json({ message: "Can't find this user" });
     }
@@ -39,11 +44,12 @@ module.exports = {
     if (!correctPw) {
       return res.status(400).json({ message: 'Wrong password!' });
     }
+
     const token = signToken(user);
     res.json({ token, user });
   },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  // user comes from `req.user` created in the auth middleware function
+
+  // Save a book to a user's `savedBooks` field
   async saveBook({ user, body }, res) {
     console.log(user);
     try {
@@ -52,22 +58,26 @@ module.exports = {
         { $addToSet: { savedBooks: body } },
         { new: true, runValidators: true }
       );
+
       return res.json(updatedUser);
     } catch (err) {
       console.log(err);
       return res.status(400).json(err);
     }
   },
-  // remove a book from `savedBooks`
+
+  // Remove a book from `savedBooks`
   async deleteBook({ user, params }, res) {
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id },
       { $pull: { savedBooks: { bookId: params.bookId } } },
       { new: true }
     );
+
     if (!updatedUser) {
       return res.status(404).json({ message: "Couldn't find user with this id!" });
     }
+
     return res.json(updatedUser);
   },
 };
