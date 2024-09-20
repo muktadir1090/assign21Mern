eefc3e763ccae3909d6a5fdf3a3dd0e4221c8330
Tuba Muktadir <muktadir1090@gmail.com>
2024-09-19 22:24:33 -0400

    bug fix

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+
+const typeDefs = `
+  type Book {
+    bookId: ID
+    title: String
+    author: [String]
+    description: String
+    image: String
+    link: String
+  }
+
+  type User {
+    username: String
+    password: String
+    email: String
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    bookId: String
+    title: String
+    author: [String]
+    description: String
+    image: String
+    link: String
+  }
+
+  type Query {
+  
+    me: User
+  }
+
+  type Mutation {
+    saveBook(data: BookInput): User
+    deleteBook(id: ID): User
+    login(username: String, email: String): Auth
+    addUser(username: String, email: String, password: String): Auth
+  }
+`;
+
+module.exports = typeDefs;
